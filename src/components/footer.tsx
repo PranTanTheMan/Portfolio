2c22f4a595bf7f18bdd9417345bd4aa86c80c8f8
PranTanTheMan <Seriousbro23@gmail.com>
2022-12-06 23:49:23 -0600

    Footer V1

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,29 @@
+import React from "react";
+import { Flex, Text } from "@chakra-ui/react";
+
 export default function Footer() {
   return (
     <>
-      <div>Footer</div>
+      <Flex alignItems="center" justifyContent="center">
+        <Flex
+          w="100%"
+          as="footer"
+          flexDir={{
+            base: "column",
+            sm: "row",
+          }}
+          align="center"
+          justify="center"
+          height={"auto"}
+          py={7}
+          m={0}
+        >
+          <Text textAlign={"center"} fontSize={"18px"} fontWeight={"400"}>
+            Made with Next.Js, ChakraUI, and Typescript by
+            <strong> Pranith Molakalapalli 💜</strong>
+          </Text>
+        </Flex>
+      </Flex>
     </>
   );
 }
