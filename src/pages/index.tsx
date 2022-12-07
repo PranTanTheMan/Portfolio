8e0ec399ed0b31be961fabe9f9f516685940dd28
PranTanTheMan <Seriousbro23@gmail.com>
2022-12-06 23:50:39 -0600

    Index V1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,17 @@
 import Hero from "@components/hero";
 import About from "@components/about";
+import Work from "@components/work";
+import Experience from "@components/experience";
+import Contact from "@components/contact";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <About />
+      <Work />
+      <Experience />
+      <Contact />
     </>
   );
 }
