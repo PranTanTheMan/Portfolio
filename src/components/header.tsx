17c2f60ed9617cd855be7a960f970afecb659558
PranTanTheMan <Seriousbro23@gmail.com>
2022-12-06 23:49:30 -0600

    Needs Work

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,7 @@
-import { useState } from "react";
-import { useColorMode, Switch, Flex, Button } from "@chakra-ui/react";
-import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
-import NextLink from "next/link";
+// import { useState } from "react";
+// import { useColorMode, Switch, Flex, Button } from "@chakra-ui/react";
+// import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
+// import NextLink from "next/link";
 
 export default function Main() {
   return (
